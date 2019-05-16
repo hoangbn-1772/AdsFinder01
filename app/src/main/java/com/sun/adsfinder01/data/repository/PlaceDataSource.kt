099c70e0127477929fdc96b1c74c7f646b44472c
bui.ngoc.hoang@framgia.com <buihoangbg01@gmail.com>
2019-05-16 14:22:10 +0700

    Task#11886: Save or remove place

--- app/src/main/java/com/sun/adsfinder01/data/repository/PlaceDataSource.kt
+++ app/src/main/java/com/sun/adsfinder01/data/repository/PlaceDataSource.kt
@@ -5,4 +5,8 @@ import io.reactivex.Single
 
 interface PlaceDataSource {
     fun getPlaces(author: Int?, placeStatus: String?): Single<List<Place>>
+
+    fun savePlace(author: Int?, placeId: Int?): Single<Boolean>
+
+    fun removePlace(author: Int?, placeId: Int?): Single<Boolean>
 }
